96432ba07dd0b0caf744f52094c3893f8c161ba6
Alexandre Petibon <alexandre.petibon@outlook.fr>
2024-05-06 14:03:24 +0200

    mise en place react page home

---
+++ front/src/components/FeatureItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FeatureItem = ({ logo, altText, title, description }) => {
+    return (
+        <article className="feature-item">
+            <img src={logo} alt={altText} className="feature-icon" />
+            <h3 className="feature-item-title">{title}</h3>
+            <p>{description}</p>
+        </article>
+    );
+};
+
+export default FeatureItem;
--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -2,12 +2,37 @@ import React from 'react';
 import Footer from '../components/Footer';
 import Nav from '../components/Nav';
 import Banner from '../components/Banner';
+import Chat from '../img/icon-chat.png';
+import Money from '../img/icon-money.png';
+import Security from '../img/icon-security.png';
+import FeatureItem from '../components/FeatureItem';
 
 const Home = () => {
     return (
         <div>
             <Nav />
             <Banner />
+            <section className="features">
+        <h2 className="sr-only">Features</h2>
+        <FeatureItem
+          logo={Chat}
+          altText="Chat-Icon"
+          title="You are our #1 priority"
+          description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
+        />
+        <FeatureItem
+          logo={Money}
+          altText="Money"
+          title="More savings means higher rates"
+          description="The more you save with us, the higher your interest rate will be!"
+        />
+        <FeatureItem
+          logo={Security}
+          altText="Security"
+          title="Security you can trust"
+          description="We use top of the line encryption to make sure your data and money is always safe."
+        />
+      </section>
             <Footer />
         </div>
     );
