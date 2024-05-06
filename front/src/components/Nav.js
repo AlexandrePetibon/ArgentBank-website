da0e6462a7f3f7940e6e34d4e8948312bf708afa
Alexandre Petibon <alexandre.petibon@outlook.fr>
2024-05-06 16:17:27 +0200

    fin de la page home

--- front/src/components/Nav.js
+++ front/src/components/Nav.js
@@ -1,14 +1,15 @@
 import React from "react";
 import logo from "../img/argentBankLogo.png";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
     <header>
       <img className="logo" src={logo} alt="Logo Argent Bank" />
-      <div className="Log">
+      <NavLink to="/sign-in" className="log">
       <i className="fa fa-user-circle"></i>
           Sign In
-      </div>
+      </NavLink>
     </header>
   );
 };
